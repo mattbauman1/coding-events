1b667a415739ca37ff2c23b612393dfefda52d84
mattbauman <mattbauman1@gmail.com>
2019-12-11 21:00:45 -0600

    Hashmap Working!

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,13 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static Map<String, String> events = new HashMap<>();
 
     // Lives at /events
     @GetMapping
@@ -30,8 +32,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
+        events.putIfAbsent(eventName, eventDescription);
         return "redirect:";
     }
 
